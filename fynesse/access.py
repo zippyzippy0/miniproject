9228ee2c5962da911d6eef6184d58ee528213c18
zippyzippy0 <zkavutha403@gmail.com>
2025-09-17 15:11:15 +0300

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -38,8 +38,12 @@ def download_file(url: str, save_path: str) -> str:
 
 
 def load_osm_data(place: str, tags: dict):
-    gdf = ox.geometries_from_place(place, tags)
+    try:
+        gdf = ox.geometries_from_place(place, tags)
+    except AttributeError:
+        gdf = ox.pois_from_place(place, tags)
     return gdf
+
 
 
 def init_hdx(user_agent: str = "fynesse_project"):
